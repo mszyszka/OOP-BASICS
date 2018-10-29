0b776fd08870df9c52af9ef97ee9f06aa0b37ef9
Michal <mszyszka223@gmail.com>
2018-10-29 16:52:36 +0100

    I create Object by factory function and constructor function

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 //Object define with object literal {} 
+//It can be problematic when object has methods
 const circle = {
     //property
     radius: 1,
@@ -11,8 +12,35 @@ const circle = {
     },
     //method
     draw: function() {
-        console.log('draw');
+        console.log('Drawing circle with object literal {}');
     }
 };
 
 circle.draw();
+
+//Factory function
+//Factory function is a function that return an Object
+//they offer the ability to easily produce obhect instances 
+//without diving into the complexities of classes and the new keyword
+function createCircle(radius) {
+    return {
+        radius: radius,
+        draw: function() {
+            console.log('Drawing circle with radius: ' + radius + ', with factory function');
+        }
+    };
+}
+
+const myCircle = createCircle(1);
+myCircle.draw();
+
+//Constructor Function
+function Circle(radius) {
+    this.radius = radius;  
+    this.draw = function() {
+        console.log('Drawing circle with constructor function');
+    }
+}
+
+const anotherCircle = new Circle(1);
+anotherCircle.draw();
