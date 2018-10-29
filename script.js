f164c81f255f0fc497d153d6fe55784e02320974
Michal <mszyszka223@gmail.com>
2018-10-29 14:17:58 +0100

    I create object with object literal {}

---
+++ script.js
@@ -0,0 +1,18 @@
+//Object define with object literal {} 
+const circle = {
+    //property
+    radius: 1,
+    //object
+    location: {
+        //property
+        x:1,
+        //property
+        y: 1
+    },
+    //method
+    draw: function() {
+        console.log('draw');
+    }
+};
+
+circle.draw();
